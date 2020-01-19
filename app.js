68eb295ebb01e58a551f445a340615407917b8ad
Sandra Schneider <san.schneider@ostfalia.de>
2020-01-19 15:10:04 +0100

    Validating User Input
    + add validator für input fields

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 
-cancelBtn.addEventListener('click', () => {
-    console.log("It works!");
+confirmBtn.addEventListener('click', () => {
+    const enteredReason = reasonInput.value;
+    const enteredAmount = amountInput.value;
+
+    if (enteredReason.trim().length <= 0 || enteredAmount.trim().length <= 0 || enteredAmount <= 0) {
+        return;
+    }
+    console.log(enteredReason, enteredAmount);
 });
