da63994a9c89498a642c76f6f379b85721291214
Keiko <schneidersan@yahoo.de>
2020-01-20 17:37:12 +0100

    Creating Ionic Elements Programmatically
    + add list output with Ionic element

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
 
 confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
@@ -10,5 +11,8 @@ confirmBtn.addEventListener('click', () => {
     if (enteredReason.trim().length <= 0 || enteredAmount.trim().length <= 0 || enteredAmount <= 0) {
         return;
     }
-    console.log(enteredReason, enteredAmount);
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+
+    expensesList.appendChild(newItem);
 });
